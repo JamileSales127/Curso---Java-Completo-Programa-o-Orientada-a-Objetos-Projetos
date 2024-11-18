fb293fffbe53c0b1ecbf098a75e2721e693a30fb
JamileSales127 <jjsales@uolinc.com>
2024-11-18 13:04:06 -0300

    Exercicio de fixação: conversorDolar

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import entites.ExercicioConversorDolar.Conversor;
 import entites.Funcionario;
 import java.util.Scanner;
 
@@ -7,7 +8,15 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    Conversor conversor = new Conversor();
 
+    System.out.println("What is the dollar price?");
+    conversor.setCotacao(sc.nextDouble());
+
+    System.out.println("How many dollars will be bought?");
+    conversor.setValor(sc.nextDouble());
+
+    System.out.println("Amount to be paid in reais = " + conversor.converter(conversor.getValor(), conversor.getCotacao()));
     sc.close();
   }
 }
