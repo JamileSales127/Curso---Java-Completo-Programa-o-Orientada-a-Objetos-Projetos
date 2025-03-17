1ce46c75e057c6eb182e9ee4539ede5a1470c231
jjsales <jamilesales083@gmail.com>
2025-03-17 10:09:08 -0300

    conteúdo desnecessário

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,6 @@
-import entites.ExercicioConversorDolar.Conversor;
-import entites.Funcionario;
-import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    Conversor conversor = new Conversor();
-
-    System.out.println("What is the dollar price?");
-    conversor.setCotacao(sc.nextDouble());
-
-    System.out.println("How many dollars will be bought?");
-    conversor.setValor(sc.nextDouble());
-
-    System.out.println("Amount to be paid in reais = " + conversor.converter(conversor.getValor(), conversor.getCotacao()));
-    sc.close();
   }
 }
